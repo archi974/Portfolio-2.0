06fde82721490bda5164d193f053e2c08a9148ab
Vincent <vincent.k.97480@gmail.com>
2023-08-01 18:50:21 +0200

    [UPDATE] base contact with css

---
+++ src/components/googleMap.jsx
@@ -0,0 +1,12 @@
+const GoogleMapComponent = () => {
+    // lat: 48.8071138, lng: 2.4761018;
+
+    return (
+        <div className="bloc-map">
+            <img src="https://cdn.discordapp.com/attachments/1081227920770596865/1135974699835346974/Capture_decran_2023-08-01_a_18.36.52.png" alt="my home" />
+        </div>
+    );
+
+}
+
+export default GoogleMapComponent;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,22 @@
 import Layout from '../components/layout';
+import GoogleMapComponent from '../components/googleMap';
 
 export default function Contact() {
 
     return (
         <Layout>
-            <p>CONTACT</p>
+            <h1>Contact.</h1>
+            <section>
+                <h2>ADDRESS</h2>
+                <p>75 avenue</p>
+                <p>saint maur-des-fossées</p>
+                <p>94100</p>
+            </section>
+            <section class="contact">
+                <h2>CONTACT</h2>
+                <p>vincent.k.97480@gmail.com</p>
+            </section>
+            <GoogleMapComponent />
         </Layout>
     )
 }
