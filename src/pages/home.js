db52011942a4e0513bbf40ec7586e439b07f7e8a
Vincent <vincent.k.97480@gmail.com>
2023-08-02 14:54:32 +0200

    [UPDATE] css for home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,11 +4,13 @@ export default function Home() {
 
     return (
         <>
-            <p>Portfolio</p>
-            <NavLink to="/project">Projects</NavLink>
-            <NavLink to="/skills">Skills</NavLink>
-            <NavLink to="/contact">Contact</NavLink>
-            <p>Vincent K/BIDI © 2023</p>
+            <div className="home-menu">
+                <p>Portfolio</p>
+                <NavLink to="/project">Projects</NavLink>
+                <NavLink to="/skills">Skills</NavLink>
+                <NavLink to="/contact">Contact</NavLink>
+            </div>
+            <p className="copyright-home">Vincent K/BIDI © 2023</p>
         </>
     )
 }
