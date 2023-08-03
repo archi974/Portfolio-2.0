25535b301f76200742d8e18db5dab1fc7123ca3b
Vincent <vincent.k.97480@gmail.com>
2023-08-03 15:27:22 +0200

    [UPDATE/FIX] api academic description and google map

--- src/components/googleMap.jsx
+++ src/components/googleMap.jsx
@@ -1,28 +1,32 @@
 import React from "react";
 import GoogleMapReact from 'google-map-react';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const AnyReactComponent = ({ text }) => <div className="map-pointer">{text}</div>;
 
-export default function SimpleMap(){
-  const defaultProps = {
-    center: {
-      lat: 48.8071138,
-      lng: 2.4761018
-    },
-    zoom: 15
-  };
+const GoogleMapComponent = () =>{
+    const defaultProps = {
+        center: {
+            lat: 48.8071138,
+            lng: 2.4761018
+        },
+        zoom: 15
+    };
 
-  return (
-    <div style={{ height: '100vh', width: '100%' }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyDkASREEkNjzHMTF0iXcp2dpP0s1bHJlpM" }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
-      >
-        <AnyReactComponent
-          text="I'm here"
-        />
-      </GoogleMapReact>
-    </div>
-  );
+    return (
+        <div className="bloc-map">
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: "AIzaSyDkASREEkNjzHMTF0iXcp2dpP0s1bHJlpM" }}
+                defaultCenter={defaultProps.center}
+                defaultZoom={defaultProps.zoom}
+            >
+                <AnyReactComponent
+                    text="I'm here"
+                    lat= {48.8071138}
+                    lng={2.4761018}
+                />
+            </GoogleMapReact>
+        </div>
+    );
 }
+
+export default GoogleMapComponent;
