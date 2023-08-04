eb9fc5f98f33aef67fe5b26da4715794f6163ba3
Vincent <vincent.k.97480@gmail.com>
2023-08-04 19:51:44 +0200

    [UPDATE] hover effect and style css project

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,17 +1,37 @@
+import React, { useState } from 'react';
 import Layout from '../components/layout';
 import projects from '../fixture/projects.json'
 
 export default function Projects() {
+    const [hoveredProject, setHoveredProject] = useState(null);
 
     return (
         <Layout>
             <article className="bloc-projects">
                 <h1>Projects</h1>
-                {projects.map((project, i) => (
-                    <div className={`projet ${i % 2 === 0 ? 'left' : 'right'}`} key={i}>
-                        <img src={project.medias.desktop.image} alt="home page" />
-                    </div>
-                ))}
+                <div className="bloc-projects-container">
+                    {projects.map((project, i) => (
+                        <section
+                            className={`projet ${i % 2 === 0 ? 'left' : 'right'}`}
+                            key={i}
+                        >
+                            {hoveredProject === project ? (
+                                <article className="project-description">
+                                    <h2>{project.name}</h2>
+                                    <p>{project.description}</p>
+                                </article>
+                            ) : (
+                                <article
+                                    className="project-image"
+                                    onMouseEnter={() => setHoveredProject(project)}
+                                    onMouseLeave={() => setHoveredProject(null)}
+                                >
+                                    <img src={project.medias.desktop.image} alt="home page" />
+                                </article>
+                            )}
+                        </section>
+                    ))}
+                </div>
             </article>
         </Layout>
     )
