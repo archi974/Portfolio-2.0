fa18a50533246598531c86bdf1810f14e09fbeef
Vincent <vincent.k.97480@gmail.com>
2023-08-08 17:32:21 +0200

    [UPDATE] modal responsive

--- src/components/projectModal.jsx
+++ src/components/projectModal.jsx
@@ -19,13 +19,16 @@ const ProjectModal = ({ showModal, closeModal, project }) => {
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12z" /></svg>
             </button>
             <h2 className="modal-title">{project?.name}</h2>
-            {
-                project?.medias.desktop.video && (
-                    <iframe width='560' height='320' src={project?.medias.desktop.video} title={project?.name} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;" allowfullscreen></iframe>
-                )
-            }
+            <div className='modal-content-video'>
+                {
+                    project?.medias.desktop.video && (
+                        <iframe className="modal-video" src={project?.medias.desktop.video} title={project?.name} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;" allowFullScreen></iframe>
+                    )
+                }
+            </div>
             <article className="modal-description">
                 <p>{project?.year}</p>
+                <h3>Description</h3>
                 <p>{project?.description}</p>
                 <p>Langages utilisés : {project?.language.join(', ')}</p>
                 <h3>Contrainte rencontrée</h3>
