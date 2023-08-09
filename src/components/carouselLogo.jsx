4752c30b24e5fd25ff4b8f3558a5dba2a36aaedf
Vincent <vincent.k.97480@gmail.com>
2023-08-09 17:22:31 +0200

    [UPDATE] button carousel

--- src/components/carouselLogo.jsx
+++ src/components/carouselLogo.jsx
@@ -34,18 +34,18 @@ const LogoCarousel = () => {
 
     return (
         <div className="logo-carousel">
-            <button className="logo-button left" onClick={handlePrev}><svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" viewBox="0 0 24 24"><path d="m14 18l-6-6l6-6l1.4 1.4l-4.6 4.6l4.6 4.6L14 18Z" /></svg></button>
             <div className="all-logo-title">
                 <div className="logo-title">
                     <h2>{slideTitles[currentIndex]}</h2>
                 </div>
                 <div className="all-logo">
+                    <button className="logo-button left" onClick={handlePrev}><svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" viewBox="0 0 24 24"><path d="m14 18l-6-6l6-6l1.4 1.4l-4.6 4.6l4.6 4.6L14 18Z" /></svg></button>
                     {currentLogos.map((logo, index) => (
                         <div className="logo" key={index} dangerouslySetInnerHTML={{ __html: logo }} />
                     ))}
+                    <button className="logo-button right" onClick={handleNext}><svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" viewBox="0 0 24 24"><path d="M12.6 12L8 7.4L9.4 6l6 6l-6 6L8 16.6l4.6-4.6Z" /></svg></button>
                 </div>
             </div>
-            <button className="logo-button right" onClick={handleNext}><svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" viewBox="0 0 24 24"><path d="M12.6 12L8 7.4L9.4 6l6 6l-6 6L8 16.6l4.6-4.6Z" /></svg></button>
         </div>
     );
 };
