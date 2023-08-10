2472c09385ab81f0ed1ad2fbc1908e18760f3ed6
Vincent <vincent.k.97480@gmail.com>
2023-08-10 16:08:08 +0200

    [UPDATE] contact and home responsive

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,14 +4,16 @@ export default function Home() {
 
     return (
         <>
-            <div className="home-menu">
-                <p>Portfolio</p>
-                <NavLink to="/project">Projects</NavLink>
-                <NavLink to="/skills">Skills</NavLink>
-                <NavLink to="/contact">Contact</NavLink>
-                <NavLink to="/about-me">About me</NavLink>
+            <div className="bloc-home">
+                <section className="home-menu">
+                    <p>Portfolio</p>
+                    <NavLink to="/project">Projects</NavLink>
+                    <NavLink to="/skills">Skills</NavLink>
+                    <NavLink to="/contact">Contact</NavLink>
+                    <NavLink to="/about-me">About me</NavLink>
+                </section>
+                <p className="copyright-home">Vincent K/BIDI © 2023</p>
             </div>
-            <p className="copyright-home">Vincent K/BIDI © 2023</p>
         </>
     )
 }
