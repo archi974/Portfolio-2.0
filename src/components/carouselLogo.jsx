3cac1223b9855f86ac263eba647cd19d21347aa1
Vincent <vincent.k.97480@gmail.com>
2023-08-11 18:32:01 +0200

    [FIX] warning handleNext, accessibility button

--- src/components/carouselLogo.jsx
+++ src/components/carouselLogo.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import skills from '../fixture/skills.json';
 
 const chunkArray = (array, size) => {
@@ -16,13 +16,13 @@ const LogoCarousel = () => {
     const currentLogos = chunkedLogos[currentIndex];
     const slideTitles = ["Langages", "Outils"];
 
-    const handlePrev = () => {
+    const handlePrev = useCallback(() => {
         setCurrentIndex((prevIndex) => (prevIndex === 0 ? chunkedLogos.length - 1 : prevIndex - 1));
-    };
+    }, [chunkedLogos]);
 
-    const handleNext = () => {
+    const handleNext = useCallback(() => {
         setCurrentIndex((prevIndex) => (prevIndex === chunkedLogos.length - 1 ? 0 : prevIndex + 1));
-    };
+    }, [chunkedLogos]);
 
     useEffect(() => {
         const interval = setInterval(handleNext, 5000);
@@ -30,7 +30,7 @@ const LogoCarousel = () => {
         return () => {
             clearInterval(interval);
         };
-    }, []); // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [handleNext]);
 
     return (
         <div className="logo-carousel">
@@ -39,11 +39,11 @@ const LogoCarousel = () => {
                     <h2>{slideTitles[currentIndex]}</h2>
                 </div>
                 <div className="all-logo">
-                    <button className="logo-button" onClick={handlePrev}><svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" viewBox="0 0 24 24"><path d="m14 18l-6-6l6-6l1.4 1.4l-4.6 4.6l4.6 4.6L14 18Z" /></svg></button>
+                    <button className="logo-button" onClick={handlePrev}  aria-label="Diapositive précédente"><svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" viewBox="0 0 24 24"><path d="m14 18l-6-6l6-6l1.4 1.4l-4.6 4.6l4.6 4.6L14 18Z" /></svg></button>
                     {currentLogos.map((logo, index) => (
                         <div className="logo" key={index} dangerouslySetInnerHTML={{ __html: logo }} />
                     ))}
-                    <button className="logo-button" onClick={handleNext}><svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" viewBox="0 0 24 24"><path d="M12.6 12L8 7.4L9.4 6l6 6l-6 6L8 16.6l4.6-4.6Z" /></svg></button>
+                    <button className="logo-button" onClick={handleNext}  aria-label="Diapositive suivante"><svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" viewBox="0 0 24 24"><path d="M12.6 12L8 7.4L9.4 6l6 6l-6 6L8 16.6l4.6-4.6Z" /></svg></button>
                 </div>
             </div>
         </div>
