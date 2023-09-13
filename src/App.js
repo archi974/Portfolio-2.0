1c00b251bef13f239a89377b078cd58601b9b7c4
Vincent <vincent.k.97480@gmail.com>
2023-09-13 16:48:19 +0200

    [UPDATE/ADD] html lang, professional competence

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
 
   useEffect(() => {
     localStorage.setItem('language', language);
+    document.querySelector('html').lang = language;
   }, [language]);
 
   const toggleLanguage = () => {
