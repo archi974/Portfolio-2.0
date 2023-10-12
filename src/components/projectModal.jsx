d2d7d3567c983c7b5cb92c89970eae633551f82d
Vincent <vincent.k.97480@gmail.com>
2023-10-12 12:57:12 +0200

    [UPDATE] card project and modal

--- src/components/projectModal.jsx
+++ src/components/projectModal.jsx
@@ -18,7 +18,7 @@ const ProjectModal = ({ showModal, closeModal, project }) => {
             <button onClick={closeModal} className="modal-close-button">
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12z" /></svg>
             </button>
-            <h2 className="modal-title">{project?.name}</h2>
+            {/* <h2 className="modal-title">{project?.name}</h2> */}
             <div className='modal-content-video'>
                 {
                     project?.medias.desktop.video && (
@@ -26,15 +26,13 @@ const ProjectModal = ({ showModal, closeModal, project }) => {
                     )
                 }
             </div>
-            <article className="modal-content-text">
+            {/* <article className="modal-content-text">
                 <p className="modal-year">{project?.year}</p>
                 <section className="modal-description">
                     <h3>Description :</h3>
                     <p>{project?.description}</p>
-                    <p>Languages used : {project?.language.join(', ')}</p>
                 </section>
                 <section className="modal-contraints">
-                    <h3>{project?.titleConstraints} :</h3>
                     {
                         project?.constraints.map((issue, j) => (
                             <ul key={j}>
@@ -52,7 +50,7 @@ const ProjectModal = ({ showModal, closeModal, project }) => {
                         <path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112v320c0 44.2 35.8 80 80 80h320c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v112c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16h112c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z" />
                     </svg>
                 </a>
-            </div>
+            </div> */}
         </Modal>
     );
 };
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,28 +3,13 @@ import ProjectModal from '../components/projectModal';
 import projectsFixture from '../fixture/projects.json';
 
 export default function Projects({ language }) {
-    const [hoveredProjects, setHoveredProjects] = useState({});
     const [showModal, setShowModal] = useState(false);
-    // const [selectedProject, setSelectedProject] = useState(null);
+    const [selectedProject, setSelectedProject] = useState(null);
 
-    const handleDescriptionMouseEnter = (project) => {
-        setHoveredProjects((prevHoveredProjects) => ({
-            ...prevHoveredProjects,
-            [project.name]: true,
-        }));
+    const openModal = (project) => {
+        setShowModal(true);
+        setSelectedProject(project)
     };
-
-    const handleDescriptionMouseLeave = (project) => {
-        setHoveredProjects((prevHoveredProjects) => ({
-            ...prevHoveredProjects,
-            [project.name]: false,
-        }));
-    };
-
-    // const openModal = (project) => {
-    //     setShowModal(true);
-    //     setSelectedProject(project)
-    // };
 
     const closeModal = () => {
         setShowModal(false);
@@ -41,43 +26,49 @@ export default function Projects({ language }) {
                 <div className="bloc-projects-container">
                     {projectsFixture[language].map((project, i) => (
                         <section
-                            className={`projet-card ${i % 2 === 0 ? 'left' : 'right'} ${hoveredProjects[project.name] ? 'hovered' : ''
-                                }`}
+                            className={`projet-card`}
                             key={i}
                         >
-                            {hoveredProjects[project.name] ? (
-                                <article
-                                    className={`project-description ${hoveredProjects[project.name] ? 'hovered' : ''}`}
-                                    onMouseLeave={() => handleDescriptionMouseLeave(project)}
-                                    // onClick={() => openModal(project)}
-                                >
-                                    <h2>{project.name}</h2>
-                                    <p>{project.description}</p>
-                                    <div className="gitHub-bloc">
-                                        <a href={project?.link} className="gitHub-link" target="_blank" rel="noreferrer">
-                                            <p>GitHub</p>
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 512 512">
-                                                <path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112v320c0 44.2 35.8 80 80 80h320c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v112c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16h112c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z" />
-                                            </svg>
-                                        </a>
-                                    </div>
-                                </article>
-                            ) : (
-                                <article
-                                    className={`project-image ${hoveredProjects[project.name] ? 'hovered' : ''}`}
-                                    onMouseEnter={() => handleDescriptionMouseEnter(project)}
-                                >
-                                    <img src={project.medias.desktop.imgSrc} alt={project.medias.desktop.imgAlt} srcSet={project.medias.mobile.imgSrc} width={500} height={286} />
-                                </article>
-                            )}
+                            <article
+                                className={`project-description`}
+                            >
+                                <p className='project-date'>{project.year}</p>
+                                <h2>{project.name}</h2>
+                                <h3>Description :</h3>
+                                <p>{project.description}</p>
+                                <h3>{projectsFixture[language][0].languageTitle} :</h3>
+                                <div className="project-logo">
+                                    {project.language.map((logo, index) => (
+                                        <div key={index} className="project-logo-svg" dangerouslySetInnerHTML={{ __html: logo }} />
+                                    ))}
+                                </div>
+                                <div className="gitHub-bloc">
+                                    <a href={project?.link} className="gitHub-link" target="_blank" rel="noreferrer">
+                                        <p>GitHub</p>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 512 512">
+                                            <path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112v320c0 44.2 35.8 80 80 80h320c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v112c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16h112c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z" />
+                                        </svg>
+                                    </a>
+                                </div>
+                            </article>
+                            <article
+                                className={`project-image`}
+                            >
+                                <button className="btn-play-modal" onClick={() => openModal(project)}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24">
+                                        <path d="M8 6.82v10.36c0 .79.87 1.27 1.54.84l8.14-5.18a1 1 0 0 0 0-1.69L9.54 5.98A.998.998 0 0 0 8 6.82z" />
+                                    </svg>
+                                </button>
+                                <img src={project.medias.desktop.imgSrc} alt={project.medias.desktop.imgAlt} srcSet={project.medias.mobile.imgSrc} width={500} height={286} />
+                            </article>
                         </section>
                     ))}
                 </div>
-            </article>
+            </article >
             <ProjectModal
                 showModal={showModal}
                 closeModal={closeModal}
-                // project={selectedProject}
+                project={selectedProject}
             />
         </>
     )
