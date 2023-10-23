4186fbfe4ec4d02289926eb1107b1657d9f1f2cc
Vincent <vincent.k.97480@gmail.com>
2023-10-23 19:42:30 +0200

    [UPDATE] restore last version

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,9 +5,6 @@ import projectsFixture from '../fixture/projects.json';
 export default function Projects({ language }) {
     const [showModal, setShowModal] = useState(false);
     const [selectedProject, setSelectedProject] = useState(null);
-    const [scrollPosition, setScrollPosition] = useState(0);
-
-    const breakPoints = projectsFixture[language].map((_, index) => (index + 1) * 500);
 
     const openModal = (project) => {
         setShowModal(true);
@@ -19,14 +16,7 @@ export default function Projects({ language }) {
     };
 
     useEffect(() => {
-        window.scrollTo(0, 0)
-        const handleScroll = () => {
-            setScrollPosition(window.scrollY);
-        };
-        window.addEventListener("scroll", handleScroll);
-        return () => {
-            window.removeEventListener("scroll", handleScroll)
-        }
+        window.scrollTo(0, 0);
     }, [])
 
     return (
@@ -36,7 +26,7 @@ export default function Projects({ language }) {
                 <div className="bloc-projects-container">
                     {projectsFixture[language].map((project, i) => (
                         <section
-                            className={`project-card ${scrollPosition > breakPoints[i] ? "project-card-hidden" : ""}`}
+                            className="project-card"
                             key={i}
                         >
                             <article
