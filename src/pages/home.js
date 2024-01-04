676fcd382c5b66f035e3e99197f192bfc480bcef
Vincent <vincent.k.97480@gmail.com>
2024-01-05 00:19:46 +0100

    [UPDATE/FIX] variable and home before

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,16 +5,14 @@ export default function Home({ language }) {
     const routeArray = ["/project", "/skills", "/contact", "/about-me"];
 
     return (
-        <div className="bloc-home">
-            <section className="home-menu">
-                <p>{other.descriptionHome}</p>
-                {
-                    routeArray.map((route, i) => (
-                        <NavLink key={i} to={route}>{other[language].titleHome[i]}</NavLink>
-                    ))
-                }
-            </section>
+        <section className="bloc-home">
+            <h1>{other.descriptionHome}</h1>
+            {
+                routeArray.map((route, i) => (
+                    <NavLink key={i} to={route}>{other[language].titleHome[i]}</NavLink>
+                ))
+            }
             <p className="copyright-home">Vincent K/BIDI © 2023</p>
-        </div>
+        </section>
     )
 }
