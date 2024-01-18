c43237f601d000bfef65bb2d85ac84ce05b4e59e
Vincent <vincent.k.97480@gmail.com>
2024-01-18 17:29:34 +0100

    [FIX/UPDATE] h1 anim and style color button

--- src/components/autoTypingText.jsx
+++ src/components/autoTypingText.jsx
@@ -1,44 +1,22 @@
 import React, { useState, useEffect } from 'react';
 
-const Typing = ({ text, speed = 400, loop = true }) => {
+const AutoTypingText = ({ text }) => {
   const [displayedText, setDisplayedText] = useState('');
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [isDeleting, setIsDeleting] = useState(false);
-  
+
   useEffect(() => {
-    const typingTimeout = setTimeout(() => {
-      if (!isDeleting) {
-        // Ajoute un caractère au texte affiché
+    if (currentIndex < text.length) {
+      const typingTimeout = setTimeout(() => {
         setDisplayedText((prevText) => prevText + text[currentIndex]);
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % text.length);
+        setCurrentIndex((prevIndex) => prevIndex + 1);
+      }, 100); // Délai entre chaque ajout de caractère (en millisecondes)
 
-        if (currentIndex === text.length - 1) {
-          // Commence à effacer après avoir affiché tout le texte
-          setIsDeleting(true);
-        }
-      } else {
-        // Enlève un caractère du texte affiché
-        setDisplayedText((prevText) => prevText.slice(0, -1));
+      // Nettoyer le timeout lorsque le composant est démonté ou lorsque tout le texte a été affiché
+      return () => clearTimeout(typingTimeout);
+    }
+  }, [currentIndex, text]);
 
-        if (displayedText === '') {
-          // Réinitialise l'index après avoir effacé tout le texte
-          setCurrentIndex(0);
-          setIsDeleting(false);
-
-          if (!loop) {
-            // Arrête le typage lorsque tout le texte a été effacé et la boucle n'est pas activée
-            clearTimeout(typingTimeout);
-          }
-        }
-      }
-    }, isDeleting ? speed / 2 : speed); // Délai entre chaque ajout ou suppression de caractère (en millisecondes)
-
-    // Nettoyer le timeout lorsque le composant est démonté
-    return () => clearTimeout(typingTimeout);
-  }, [currentIndex, text, speed, loop, isDeleting, displayedText]);
-
-  return (<h1>{displayedText}</h1>)
-
+  return <h1>{displayedText}</h1>;
 };
 
-export default Typing;
+export default AutoTypingText;
