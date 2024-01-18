fd4e5bc05b6a1219c31b36d8ff8f6935a4045072
Vincent <vincent.k.97480@gmail.com>
2024-01-18 17:19:48 +0100

    [UPDATE] notfound style page

--- src/pages/notFoundPage.js
+++ src/pages/notFoundPage.js
@@ -1,10 +1,12 @@
 import other from "../fixture/other.json";
+import AutoTypingText from '../components/autoTypingText';
 
 export default function NotFoundPage({ language }) {
+    const textToDisplay = "404";
 
     return (
         <article className="bloc-notfound">
-            <h1>404</h1>
+            <AutoTypingText text={textToDisplay} />
             <p>{other[language].descriptionNotFound}</p>
         </article>
     )
